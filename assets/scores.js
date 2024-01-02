5a946f702298a308530a8f87eec608704bfdcc45
MoskaDowlatzay <moskadowlatzay@hotmail.com>
2024-01-02 15:35:03 +0000

    code added

--- assets/scores.js
+++ assets/scores.js
@@ -6,3 +6,13 @@ document.addEventListener("DOMContentLoaded", function() {
       if (highscoresList) {
         const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
         highScores.sort((a, b) => b.score - a.score);
+
+ // Display high scores
+      highscoresList.innerHTML = "";
+      highScores.forEach(score => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${score.initials}: ${score.score}`;
+        highscoresList.appendChild(listItem);
+      });
+  }
+}
